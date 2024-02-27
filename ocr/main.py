c9f95daad26dd9028e722e65dc9350ed3391da2d
demidovichpavel <demidovicpav4@gmail.com>
2024-02-27 13:33:32 +0300

    small fix

--- ocr/main.py
+++ ocr/main.py
@@ -4,12 +4,11 @@ import urllib.request
 import ssl
 
 from io import BytesIO
-from paddleocr import PaddleOCR
+from easyocr import Reader
 from multiprocessing import Pool
 from PIL import Image
 
-
-ocr = PaddleOCR(use_angle_cls=True, lang='en', use_gpu=False)
+reader = Reader(['en'])
 
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
@@ -25,64 +24,53 @@ def process_img(img_urls: str = 'https://i.postimg.cc/BQV7JDh7/29-23-18-29-s.jpg
     img = img.convert('L')
     img = np.array(img)
 
-    result = ocr.ocr(img, cls=True)
+    result = reader.readtext(img)
 
     two_cords = []
     coordinates = {}
 
     for line in result:
-        for word_info in line:
-            word = word_info[1][0]
-            coord = re.findall(r'(\s*\d{2,}\.\d{4,6}\s*)', word)
-            print(word)
-            try:
-                if 'N' in word and len(word) >= 15:
-                    # Для координат типа 404956184N39.71779004E+5,90m
-                    coords = word.split('N')
-                    ready_cords = []
-                    for i in coords:
-                        i = i.replace('.', '')
-                        coord = i[0:2] + '.' + i[2:8]
-                        ready_cords.append(coord)
-                    coordinates[img_urls] = ready_cords
-                    continue
+        word = line[1]
+        coord = re.findall(r'(\d{2,}[.,]\d{4,6})', word)
+        print(word)
 
-                if len(word) >= 12 and word.count('.') == 2 and word.replace('.', '', 1).replace('.', '', 1).isnumeric() and word.count(',') == 0:
-                    for i in [word]:
-                        index_second_dot = i.find('.', i.find('.') + 1)
-                        first_coord = i[:index_second_dot - 2]
-                        second_coord = i[index_second_dot - 2:]
-                        coordinates[img_urls] = [first_coord, second_coord[:8]]
-                        continue
+        if len(coord) == 2:
+                coord[0] = coord[0].replace(',', '.')
+                coord[-1] = coord[-1].replace(',', '.')
 
-                if len(coord) == 2:
-                    coordinates[img_urls] = coord
-                    continue
-
-                if len(coord) == 1:
-                    two_cords.append(coord)
-                    continue
-            except Exception as _ex:
-                print(_ex)
+                coordinates[img_urls] = coord
                 continue
+
+        if len(coord) == 1:
+            two_cords.append(coord)
+            continue
+        try:
+            if len(word) >= 15 and word.count('.') == 2 and word.replace('.', '', 1).replace('.', '', 1).isnumeric() and word.count(',') == 0:
+                for i in [word]:
+                    index_second_dot = i.find('.', i.find('.') + 1)
+                    first_coord = i[:index_second_dot - 2]
+                    second_coord = i[index_second_dot - 2:]
+                    coordinates[img_urls] = [first_coord, second_coord[:8]]
+                    continue
+        except Exception as _ex:
+            print(_ex)
+            continue
 
     for _ in two_cords:
         ready_coords = []
         for cord in two_cords:
             ready_coords.append(*cord)
         coordinates.setdefault(img_urls, ready_coords)
-    
+
     return coordinates
 
 
 def check_img(img_urls: list) -> dict:
-    '''PaddleOCR смотрит фотографию и ищет координаты на нём'''
+    '''EasyOCR смотрит фотографию и ищет координаты на нём'''
 
     with Pool(processes=1) as pool:
         results = pool.map(process_img, img_urls)
 
     coordinates = {k: v for result in results for k, v in result.items()}
-    print(len(coordinates))
     return coordinates
 
-print(process_img())
