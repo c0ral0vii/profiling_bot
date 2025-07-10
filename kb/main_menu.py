cec5244813bf58640b64f920f92396be37ce886b
Pavel Demidovich <demidovicpav4@gmail.com>
2025-07-10 03:22:56 +0300

    create kb

--- kb/main_menu.py
+++ kb/main_menu.py
@@ -5,7 +5,7 @@ async def main_menu_keyboard(status: bool = False) -> ReplyKeyboardMarkup:
     """
     Creates a main menu keyboard with buttons for 'Start', 'Help', and 'Settings'.
     """
-    kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="Изменить тип")], [KeyboardButton(text=f"Координаты с одной цифрой: {'✅' if status else '❌'}")]], resize_keyboard=True)
+    kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="Изменить тип координат")], [KeyboardButton(text=f"Координаты с одной цифрой: {'✅' if status else '❌'}")]], resize_keyboard=True)
     
     
     return kb
