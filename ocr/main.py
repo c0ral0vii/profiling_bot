d43cd38ea0e5bf865fc4a8f3604a17e71322ec87
Pavel Demidovich <demidovicpav4@gmail.com>
2026-05-27 00:58:00 +0300

    fsad

--- ocr/main.py
+++ ocr/main.py
@@ -47,7 +47,7 @@ safe_runtime_kwargs = {
     "enable_hpi": False,
     "enable_mkldnn": False,
     "enable_cinn": False,
-    "cpu_threads": 6,
+    "cpu_threads": 8,
 }
 
 try:
@@ -527,7 +527,7 @@ task_list = set()
 async def check_img(img_urls: list, coord_status: bool = False) -> list:
     """PaddleOCR PP-OCRv5 смотрит фотографию и ищет координаты на нём"""
 
-    semaphore = asyncio.Semaphore(6)
+    semaphore = asyncio.Semaphore(4)
     tasks = [
         asyncio.create_task(process_img(img_link, semaphore, coord_status))
         for img_link in img_urls
